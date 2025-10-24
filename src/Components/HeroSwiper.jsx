0a849545b852d053a9b20b4691948a9428dc3fb8
Ruhul Amin <ruhulamin824854@gmail.com>
2025-10-25 02:33:32 +0600

    banner update and complete

--- src/Components/HeroSwiper.jsx
+++ src/Components/HeroSwiper.jsx
@@ -5,58 +5,75 @@ import { Autoplay, Navigation, Pagination } from "swiper/modules";
 import "swiper/css";
 import "swiper/css/pagination";
 import "swiper/css/navigation";
+// eslint-disable-next-line no-unused-vars
+import { motion } from "framer-motion";
 
 const HeroSwiper = () => {
   return (
     <MyContainerLayout>
-      <Swiper
-        modules={[Autoplay, Pagination, Navigation]}
-        spaceBetween={30}
-        centeredSlides={true}
-        autoplay={{
-          delay: 3000,
-          disableOnInteraction: false,
-        }}
-        pagination={{
-          clickable: true,
-        }}
-        navigation={true}
-        loop={true}
-        className="mySwiper rounded mt-1"
-      >
-        <SwiperSlide>
-          <img
-            src="/img-1.jpg"
-            alt="Slide 1"
-            className="w-full h-[350px] object-cover"
-          />
-          <div className="absolute inset-0 bg-black/40 flex items-center justify-center text-white text-xl md:text-3xl lg:text-4xl font-semibold text-center px-4">
-            Welcome to A Local Skill Exchange Platform🕺
-          </div>
-        </SwiperSlide>
+      <div className="bg-amber-300 flex flex-col md:flex-row items-center gap-6 border p-2">
+        {/* left */}
+        <motion.div
+          initial={{ x: -100, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 0.8 }}
+          className="md:w-1/2 text-center md:text-left"
+        >
+          <h1 className="text-3xl md:text-3xl lg:text-4xl font-bold mb-4">
+            Welcome to A Local Skill Exchange Platform
+          </h1>
+          <p className="text-gray-700 text-lg md:text-xl">
+            Learn, Share, and Grow Your Skills with Local Talents.
+          </p>
+        </motion.div>
+        {/* right */}
+        <motion.div
+          initial={{ x: 100, opacity: 0 }}
+          animate={{ x: 0, opacity: 1 }}
+          transition={{ duration: 0.8 }}
+          className="md:w-1/2 w-full"
+        >
+          <Swiper
+            modules={[Autoplay, Pagination, Navigation]}
+            spaceBetween={30}
+            centeredSlides={true}
+            autoplay={{
+              delay: 3000,
+              disableOnInteraction: false,
+            }}
+            pagination={{
+              clickable: true,
+            }}
+            navigation={true}
+            loop={true}
+            className="mySwiper rounded mt-1"
+          >
+            <SwiperSlide>
+              <img
+                src="/img-1.jpg"
+                alt="Slide 1"
+                className="w-full h-auto object-contain "
+              />
+            </SwiperSlide>
 
-        <SwiperSlide>
-          <img
-            src="/img-2.jpg"
-            alt="Slide 2"
-            className="w-full h-[350px] object-cover"
-          />
-          <div className="absolute inset-0 bg-black/40 flex items-center justify-center text-white text-xl md:text-3xl lg:text-4xl font-semibold text-center px-4">
-            Welcome to A Local Skill Exchange Platform🕺
-          </div>
-        </SwiperSlide>
+            <SwiperSlide>
+              <img
+                src="/img-2.jpg"
+                alt="Slide 2"
+                className="w-full h-auto object-contain flex items-center"
+              />
+            </SwiperSlide>
 
-        <SwiperSlide>
-          <img
-            src="/img-3.png"
-            alt="Slide 3"
-            className="w-full h-[350px] object-cover"
-          />
-          <div className="absolute inset-0 bg-black/40 flex items-center justify-center text-white text-xl md:text-3xl lg:text-4xl font-semibold text-center px-4">
-            Welcome to A Local Skill Exchange Platform🕺
-          </div>
-        </SwiperSlide>
-      </Swiper>
+            <SwiperSlide>
+              <img
+                src="/img-3.jpg"
+                alt="Slide 3"
+                className="w-full h-auto object-contain "
+              />
+            </SwiperSlide>
+          </Swiper>
+        </motion.div>
+      </div>
     </MyContainerLayout>
   );
 };
