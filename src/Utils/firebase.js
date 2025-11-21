cfb9797452fbeafdd4aa0b1d86fec43e425a866b
Ruhul Amin <ruhulamin824854@gmail.com>
2025-11-21 17:28:34 +0600

    env update

--- src/Utils/firebase.js
+++ src/Utils/firebase.js
@@ -5,7 +5,6 @@ import { initializeApp } from "firebase/app";
 
 import { getAuth } from "firebase/auth";
 
-
 // Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_apiKey,
