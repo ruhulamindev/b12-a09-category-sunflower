4bc13cf8bb73dc6977dd0fdd55df499fdd0675a7
Ruhul Amin <ruhulamin824854@gmail.com>
2025-11-21 16:57:59 +0600

    env file add

--- src/Utils/firebase.js
+++ src/Utils/firebase.js
@@ -8,12 +8,12 @@ import { getAuth } from "firebase/auth";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCkC7SaOcTZN0CwnM2Gr0NLv3FhTi0W9wQ",
-  authDomain: "skillswap-739e4.firebaseapp.com",
-  projectId: "skillswap-739e4",
-  storageBucket: "skillswap-739e4.firebasestorage.app",
-  messagingSenderId: "1088192730919",
-  appId: "1:1088192730919:web:18529704bf6f2e61b7fb02"
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
